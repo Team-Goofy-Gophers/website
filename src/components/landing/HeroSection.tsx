f31d3aaaf855a23ecbc5353339adbb733a92fb4a
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-08-10 17:12:04 +0530

    [style] Landing page responsive

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -3,7 +3,7 @@ import MapComponent from "./mapComponent";
 export default function HeroSection() {
   return (
     <>
-      <main className="flex h-fit w-full flex-col p-2 md:flex-row">
+      <main className="flex h-fit w-screen flex-col border-2 p-2 md:h-[calc(100vh-5rem)] md:flex-row md:gap-2">
         <div className="flex min-h-[50vh] flex-col justify-center gap-4 bg-card text-center md:w-1/2">
           <h1 className="text-4xl font-bold">Goofy Goofers</h1>
           <div className="mx-8 text-xl font-normal">
@@ -12,7 +12,7 @@ export default function HeroSection() {
           </div>
         </div>
         {/* <Map classNameName="h-screen w-full shadow-md" /> */}
-        <MapComponent className="animate-scale h-[calc(100vh-5rem)] w-full overflow-hidden rounded-md border-2 border-border" />
+        <MapComponent className="animate-scale h-[calc(100vh-5rem)] overflow-hidden rounded-md border-2 border-border md:h-full md:w-[45vw]" />
       </main>
     </>
   );
