e5caa0a16bebe8af6e11e06b763ed2266d74dee1
Prabhuomkar9 <prabhuomkar9@gmail.com>
2024-08-10 15:38:38 +0530

    [init] some shadcn components

---
+++ src/pages/admin/disaster.tsx
@@ -0,0 +1,8 @@
+import { type NextPage } from "next";
+import React from "react";
+
+const Disaster: NextPage = () => {
+  return <div>disaster</div>;
+};
+
+export default Disaster;
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,8 +1,16 @@
 import { type NextPage } from "next";
+import Link from "next/link";
 import React from "react";
 
 const Admin: NextPage = () => {
-  return <div>Admin</div>;
+  return (
+    <div>
+      Admin
+      <li>
+        <Link href="/admin/disaster">Disaster</Link>
+      </li>
+    </div>
+  );
 };
 
 export default Admin;
