6331b5127db60234ff6381278a1667b30f1c1346
Amrith R Naik <140691969+amrith-r-naik@users.noreply.github.com>
2024-08-11 04:56:28 +0530

    [styles] updates wrt to guides page

--- src/components/guideSection.tsx
+++ src/components/guideSection.tsx
@@ -12,20 +12,10 @@ import { api } from "~/utils/api";
 const GuideSection = () => {
   const { data: guides } = api.guides.getAllGuides.useQuery();
   return (
-    <div className="flex h-[calc(100vh-5rem)] w-screen flex-col items-center justify-center gap-10 bg-background p-10 px-20">
-      <h1 className="w-full text-center text-4xl font-bold">Guides</h1>
+    <div className="flex h-[calc(100vh-5rem)] w-full flex-col items-center justify-center gap-20 bg-background px-20 pb-20">
+      <h1 className="w-full text-center text-6xl font-bold">Guides</h1>
       <Carousel orientation="vertical" className="w-full">
-        <CarouselContent className="h-[300px]">
-          {/* {guides?.map((guide) => (
-            <CarouselItem>
-              <Image
-                src={guide.images[0]!}
-                alt={guide.title}
-                width={500}
-                height={500}
-              />
-            </CarouselItem>
-          ))} */}
+        <CarouselContent className="h-[400px]">
           {guides?.map((guide) => (
             <CarouselItem className="flex h-full w-full items-center justify-evenly gap-10">
               <Image
@@ -35,11 +25,11 @@ const GuideSection = () => {
                     ? guide.images[0]
                     : "/images/profile-placeholder.png"
                 }
-                alt="profile"
+                alt={guide.title}
                 width={300}
                 height={300}
               />
-              <div className="flex h-[300px] w-2/3 flex-col justify-start">
+              <div className="flex h-[400px] w-2/3 flex-col justify-start">
                 <h1 className="w-full text-center text-3xl font-bold">
                   {guide.title}
                 </h1>
